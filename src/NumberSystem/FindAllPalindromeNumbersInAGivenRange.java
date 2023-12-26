4d9d501b9cbb697709142b7840e3551ef70161b8
Subhomoy Das <subhomoydas54@gmail.com>
2023-12-27 00:48:51 +0530

    Find all Palindrome Numbers in a given range

---
+++ src/NumberSystem/FindAllPalindromeNumbersInAGivenRange.java
@@ -0,0 +1,37 @@
+package NumberSystem;
+
+import java.util.Scanner;
+
+public class FindAllPalindromeNumbersInAGivenRange {
+    static boolean isPalindrome(int n){
+        boolean check = false;
+        int rev = 0;
+        int temp = n;
+        while(n>0){
+            int ld = n%10;
+            rev = rev*10 + ld;
+            n = n/10;
+        }
+        if(rev == temp){
+            check = true;
+            return check;
+        }
+        return check;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("The number from the minimum range");
+            int min = sc.nextInt();
+            System.out.println("The number from the maximum range");
+            int max = sc.nextInt();
+            for (int i = min; i <=max ; i++) {
+                if(isPalindrome(i)){
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+        }
+        while (true);
+    }
+}
