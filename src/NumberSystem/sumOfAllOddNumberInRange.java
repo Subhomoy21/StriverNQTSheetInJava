8efdb2257dfdd238ffb4a9c0409b191e700e4e73
Subhomoy Das <subhomoydas54@gmail.com>
2024-02-14 02:01:50 +0530

    delloite feb13

---
+++ src/NumberSystem/sumOfAllOddNumberInRange.java
@@ -0,0 +1,19 @@
+package NumberSystem;
+import java.util.*;
+public class sumOfAllOddNumberInRange {
+
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the lower range:");
+      int x = sc.nextInt();
+        System.out.println("Enter the upper range:");
+      int y = sc.nextInt();
+      int sum = 0;
+        for (int i = x; i <=y ; i++) {
+            if(i%2!=0){
+                sum = sum + i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
