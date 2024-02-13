90f263f85663be00c825e33a42e56df12062fba9
Subhomoy Das <subhomoydas54@gmail.com>
2024-02-13 19:43:38 +0530

    !!!!!!!!!!!!!!!!!!!!!

---
+++ src/NumberSystem/sumOfApSeries.java
@@ -0,0 +1,27 @@
+package NumberSystem;
+
+public class sumOfApSeries {
+    // naive
+    // using the loops
+    static double sum(double a,double d,int n){
+        double sum = 0.0;
+        for (int i = 1;i<=n;i++){
+            sum = sum + a;
+            a = a + d;
+        }
+        return sum;
+    }
+    // another
+     static double sum2(double a, double d, int n) {
+        double sum=0.0;
+        for(int i=0;i<n;i++) {
+            sum+=(a+(i*d));
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(1.5,3,5));
+        System.out.println(sum2(1.5,3,5));
+    }
+}
