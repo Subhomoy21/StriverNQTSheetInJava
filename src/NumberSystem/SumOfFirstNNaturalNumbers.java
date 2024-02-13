6ccb6af3d91f9659ca852c893cbec6134ae4147c
Subhomoy Das <subhomoydas54@gmail.com>
2024-02-13 19:05:28 +0530

    sum of n natural number

---
+++ src/NumberSystem/SumOfFirstNNaturalNumbers.java
@@ -0,0 +1,21 @@
+package NumberSystem;
+
+public class SumOfFirstNNaturalNumbers {
+    // naive solution
+    static int sum1(int n){
+        int sum = 0;
+        for (int i = 1; i <= n ; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+    // optimal solution
+    static int sum2(int n){
+        int sum = (n * (n+1))/2;
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum1(5));
+        System.out.println(sum2(5));
+    }
+}
