4804d54c6682e643cae5a0060be302fb13dd742b
Subhomoy Das <subhomoydas54@gmail.com>
2024-02-14 00:28:07 +0530

    computing power

---
+++ src/NumberSystem/PowerOfANumber.java
@@ -0,0 +1,20 @@
+package NumberSystem;
+
+public class PowerOfANumber {
+    //Approach:
+    //Take a variable ans to store final answer.Initially let the value of ans be 1.
+    // Multiply ans with n for k times.
+    //Print ans.
+    static int pow(int n , int k){
+        int p = 1;
+        for (int i = 1; i <=k ; i++) {
+            p = p * n;
+        }
+        return p;
+    }
+    // other(binary exponential) -> visit selfplaced dsa github and go to iterative power(bit magics)
+
+    public static void main(String[] args) {
+        System.out.println(pow(2,3));
+    }
+}
