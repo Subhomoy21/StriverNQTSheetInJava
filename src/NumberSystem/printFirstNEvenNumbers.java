e53921c34dc18c1e012f3ba56317c5ce94e6285d
Subhomoy Das <subhomoydas54@gmail.com>
2024-02-14 01:53:58 +0530

    printFirstNEvenNumbers

---
+++ src/NumberSystem/printFirstNEvenNumbers.java
@@ -0,0 +1,16 @@
+package NumberSystem;
+import java.util.Scanner;
+public class printFirstNEvenNumbers {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        do {
+            System.out.println("Enter the number that you want to enter");
+            int n = sc.nextInt();
+            for (int i = 1; i <=n ; i++) {
+                System.out.print (2*i-2 + " ");
+            }
+            System.out.println();
+        }
+        while (true);
+    }
+}
