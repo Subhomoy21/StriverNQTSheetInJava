10c28f94aa2449590278370f9341c35f94ede175
Subhomoy Das <subhomoydas54@gmail.com>
2024-02-13 22:11:02 +0530

    fibonnaci

---
+++ src/NumberSystem/printFibonnaciuptoNthTerm.java
@@ -0,0 +1,26 @@
+package NumberSystem;
+
+public class printFibonnaciuptoNthTerm {
+    static void printFibo(int n){
+        int a = 0, b = 1;
+        if (n == 1) {
+            System.out.print(a);
+        }
+        else if (n == 2) {
+            System.out.print(a + " " + b);
+        }
+        else {
+            System.out.print(a + " " + b + " ");
+            for (int i = 3; i <= n; i++) {
+                int c = a + b;
+                System.out.print(c + " ");
+                a = b;
+                b = c;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printFibo(5);
+    }
+}
