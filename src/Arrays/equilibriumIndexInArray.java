4e3488f1516ffcb523c461b67f6a7d03e6225e8e
Subhomoy Das <subhomoydas54@gmail.com>
2024-03-06 19:57:16 +0530

    equilibrium index

--- src/Arrays/equilibriumIndexInArray.java
+++ src/Arrays/equilibriumIndexInArray.java
@@ -19,8 +19,25 @@ public class equilibriumIndexInArray {
         }
         return -1;
     }
+    // optimize solution!
+     static int findEquilibriumIdx(int nums[], int n) {
+        int totalSum = 0;
+        for (int i = 0; i < n; i++) {
+            totalSum += nums[i];
+        }
+        int leftSum = 0, rightSum = totalSum;
+        for (int i = 0; i < n; i++) {
+            rightSum -= nums[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         int[] arr = {2, 3, -1, 8, 4};
         System.out.println(findEqui(arr));
+        System.out.println(findEquilibriumIdx(arr,arr.length));
     }
 }
